188a7275f1040c236ef0140655b8aeba18d402a6
Karim Daw <karimdaw91@gmail.com>
2023-07-03 11:10:04 +0100

    Add: arctan conversion

--- ComputeMeshOrientation/src/ComputeMeshOrientation.py
+++ ComputeMeshOrientation/src/ComputeMeshOrientation.py
@@ -14,18 +14,24 @@ drag+drop 24x24 pixel png onto component. Save them under L:\TOOLS\SOURCECODE\Ic
         Version: 230629
 """
 
+import Rhino.Geometry as rg
+import math
+import time
 ghenv.Component.Name = "ComputeMeshOrientation"
 ghenv.Component.NickName = "CMO"
 ghenv.Component.Category = "KD-Tools"
 ghenv.Component.SubCategory = "Mesh"
 
-# If you want to give the component a version number etc.
-import time
-import math
-import Rhino.Geometry as rg
-ghenv.Component.Message = time.strftime("%d/%m/%Y") + "\n" + time.strftime("%H:%M:%S")
 
-# Your code here
+# If you want to give the component a version number etc.
+ghenv.Component.Message = time.strftime(
+    "%d/%m/%Y") + "\n" + time.strftime("%H:%M:%S")
+
+
+def arcTanToDegrees(arcTan):
+    degree = (arcTan * 180) / math.pi
+    degree = round(degree, 2)
+    return degree
 
 
 arcTangents = []
@@ -34,23 +40,25 @@ for i, m in enumerate(Mesh):
 
     # explicitly compute normals
     m.Normals.ComputeNormals()
-    
+    m.FaceNormals.ComputeFaceNormals()
+
     faceNormals = m.FaceNormals
     for j, norm in enumerate(faceNormals):
         xNorm = norm.X
         yNorm = norm.Y
-        arctan = math.atan2(xNorm,yNorm)
+        arctan = math.atan2(xNorm, yNorm)
         arctan = round(arctan*100)/100.0
         degrees = round(math.degrees(arctan))
-        
-        if abs(norm.Z) > 1 + FlattnessTolerance or abs(norm.Z) < 1 - FlattnessTolerance :
+
+        if abs(norm.Z) > 1 or abs(norm.Z) < 1:
 
             if abs(arctan) == 3.14:
                 arctan = 3.14
                 arcTangents.append(arctan)
             else:
                 arcTangents.append(arctan)
-        else:
-            arcTangents.append(4)
-            
+
+
 ArcTangents = arcTangents
+Orientations = map(arcTanToDegrees, arcTangents)
+print(Orientations)
